7b6ac990bda558ebc1ec5078a2dea546d6e0d02e
vjratnam03 <36513138+vjratnam03@users.noreply.github.com>
2018-02-22 07:22:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.send('article-one requested and served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
     });
 app.get('/article-two', function (req,res){
     res.send('article-two requested and served here');
